98c81171ffebd3f480068d3dce9f8684ddb3792a
Cory LaViska <cory@abeautifulsite.net>
2021-01-08 14:30:07 -0500

    chore(): update stencil and use custom elements dist

--- stencil.config.ts
+++ stencil.config.ts
@@ -8,7 +8,7 @@ export const config: Config = {
       empty: false,
     },
     {
-      type: 'dist-custom-elements-bundle',
+      type: 'dist-custom-elements',
     },
     {
       type: 'docs-readme',
